2f16e4d46c26e123c66b43e3510d34e2621f8bb2
Marc Codrington <codringtonmarc@gmail.com>
2022-08-25 18:26:45 -0300

    first commit

---
+++ application/Application.java
@@ -0,0 +1,135 @@
+package application;
+
+import model.Bottles;
+import model.Store;
+
+import java.util.Scanner;
+
+import dataStructures.DataStructures;
+
+public class Application {
+
+    private void selectorOptions() {
+        System.out.println("\n 1 add bottles in queue\n" +
+                "2. remove bottle at the top of the queue \n" +
+                "3. look at the top of the queue without removing the bottle \n" +
+                "4. check the size of the queue\n" +
+                "5. empty the queue\n" +
+                "6. enter store information\n" +
+                "7. number of deliveries \n " +
+                "8. store on the top of the list\n" +
+                "9. get the first store in the list\n" +
+                "10.get the last store in the list\n" +
+                "11.clear store list \n" +
+                "12. sort list based on orders\n" +
+                "14. randomly sort list\n" +
+                "15. exit program"
+
+        );
+    }
+
+    private Bottles bottlesInPutControls() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Fernandes Bottles Service Program.exe");
+        System.out.println("Enter Bottle information");
+        String bottleName = scanner.nextLine();
+        System.out.println("Enter the size of the bottle");
+        String bottleSize = scanner.nextLine();
+        System.out.println("Enter the prize of the bottle");
+        int bottlePrizeInSRD = scanner.nextInt();
+
+        Bottles newBottles = new Bottles(bottleName, bottleSize, bottlePrizeInSRD);
+        return newBottles;
+
+    }
+
+    private Store StoreInputControls() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter store information");
+        String storeName = scanner.nextLine();
+        System.out.println("Enter Store address");
+        String address = scanner.nextLine();
+        System.out.println("Enter Store phonenumber");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("The amount of bottles ordered");
+        int BottlesOrdered = scanner.nextInt();
+
+        Store newStore = new Store(storeName, address, phoneNumber, BottlesOrdered);
+        return newStore;
+    }
+
+    public static void main(String[] args) {
+        DataStructures dataStruct = new DataStructures();
+        Application application = new Application();
+        Scanner scanner = new Scanner(System.in);
+        int selector;
+
+        do {
+            application.selectorOptions();
+            selector = scanner.nextInt();
+            switch (selector) {
+                case 1:
+                    // enqueue bottles
+                    Bottles bottles = application.bottlesInPutControls();
+                    dataStruct.addBottlesInQueue(bottles);
+                    break;
+                case 2:
+                    // poll bottle
+                    System.out.println("remove bottle at the top of queue");
+                    dataStruct.removeBottleFromQueue();
+                    break;
+                case 3:
+                    // peek the top of the queue
+                    dataStruct.peekBottleQueue();
+                    break;
+                case 4:
+                    // size of the queue
+                    dataStruct.sizeOfBottleQueue();
+                    break;
+                case 5:
+                    // clear the queue
+                    dataStruct.emptyBottleQueue();
+                    break;
+                case 6:
+                    // enter store data
+                    Store store = application.StoreInputControls();
+                    dataStruct.addToLinkedList(store);
+                    break;
+                case 7:
+                    // number of stores in list
+                    dataStruct.sizeOfLinkedList();
+                    break;
+                case 8:
+                    // store top of the list
+                    dataStruct.peekLinkedList();
+                    break;
+                case 9:
+                    // get first store on the list
+                    dataStruct.getFirstInLinkedList();
+                    break;
+                case 10:
+                    // get last store on the list
+                    dataStruct.getLastInLinkedList();
+                    break;
+                case 11:
+                    // clear store list
+                    dataStruct.clearLinkedList();
+                    break;
+                case 12:
+                    // order list based on orders
+                    dataStruct.sortLinkedListBasedOnOrders();
+                    break;
+
+                case 13:
+                    // randomly sort list
+                    dataStruct.unsortLinkList();
+                    break;
+                case 14:
+                    // exit program
+                    System.exit(1);
+            }
+        } while (selector != 14);
+
+    }
+
+}
---
+++ dataStructures/DataStructures.java
@@ -0,0 +1,161 @@
+package dataStructures;
+
+import model.Store;
+
+import java.util.*;
+
+import model.Bottles;
+
+public class DataStructures {
+
+    private static Queue<Bottles> bottlesQueue = new LinkedList<>();
+
+    private LinkedList<Store> storeLinkedList = new LinkedList<>();
+
+    /*
+     * queue datastucture methods
+     * enqueue
+     * dequeue
+     * poll
+     * peek
+     */
+
+    // enqueue
+
+    public void addBottlesInQueue(Bottles bottles) {
+        bottlesQueue.add(bottles);
+    }
+
+    // size of the queue
+
+    public void sizeOfBottleQueue() {
+        System.out.println("Size of queue: " + bottlesQueue.size());
+        if (bottlesQueue.size() == 0) {
+            System.out.println("The queue is empty");
+        }
+    }
+
+    // dequeue
+    public void removeBottleFromQueue() {
+        bottlesQueue.poll();
+    }
+
+    // peek: look at the top without removing the item from the queue
+    public void peekBottleQueue() {
+
+        System.out.println(bottlesQueue.peek());
+    }
+    // empty queue
+
+    public void emptyBottleQueue() {
+        bottlesQueue.clear();
+        System.out.println(bottlesQueue.size());
+        System.out.println("The Queue has been cleared");
+    }
+
+    /*
+     * LinkedList datastructure methods
+     * insert
+     * clear
+     * size
+     * peek
+     * getFirst
+     * getLast
+     */
+
+    // insert
+
+    public void addToLinkedList(Store store) {
+        storeLinkedList.add(store);
+    }
+
+    public void clearLinkedList() {
+        storeLinkedList.clear();
+        System.out.println("The List has been cleared");
+        System.out.println("items in the list are = " + storeLinkedList.size());
+    }
+
+    public void sizeOfLinkedList() {
+        System.out.println("Size of the list is: " + storeLinkedList.size());
+    }
+
+    public void peekLinkedList() {
+        System.out.println("Store at the top is: " + storeLinkedList.peek());
+    }
+
+    public void getFirstInLinkedList() {
+        System.out.println("the first store on the list is: " + storeLinkedList.getFirst());
+    }
+
+    public void getLastInLinkedList() {
+        System.out.println("the store last on the list is: " + storeLinkedList.getLast());
+    }
+
+    // sorting algorithme: Using sort()
+
+    public void sortLinkedListBasedOnOrders() {
+        Collections.sort(storeLinkedList, new Store.amountOfBottlesOrdered());
+        System.out.println("Organize List based on the amount of orders");
+        for (Store s : storeLinkedList) {
+            System.out.println(s);
+        }
+    }
+
+    // sorting algorithme: shuffle sort
+
+    public void unsortLinkList() {
+        System.out.println("List before the the shuffle: " + storeLinkedList);
+        Object[] array = storeLinkedList.toArray();
+        // here we are shuffling more than once
+        // shuffle 1
+        arrayShuffle(array);
+        listDataAdder(array, storeLinkedList);
+        System.out.println("The list after shuffle 1 : "
+                + storeLinkedList.toString());
+        System.out.println();
+
+        // shuffle 2
+        arrayShuffle(array);
+        listDataAdder(array, storeLinkedList);
+        System.out.println("The list after shuffle 2 : "
+                + storeLinkedList.toString());
+        System.out.println();
+
+        // shuffle 3
+        arrayShuffle(array);
+        listDataAdder(array, storeLinkedList);
+        System.out.println("The list after shuffle 3 : "
+                + storeLinkedList.toString());
+    }
+
+    static void listDataAdder(Object[] arr, LinkedList<Store> storeLinkedList2) {
+        ListIterator<Store> it = storeLinkedList2.listIterator();
+        for (Object e : arr) {
+            it.next();
+            it.set((Store) e);
+        }
+    }
+
+    static void arrayShuffle(Object[] arr) {
+        Random rand = new Random();
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            // select index randomly
+            int index = rand.nextInt(i + 1);
+
+            // swapping between i th term and the index th
+            // term
+            Object g = arr[index];
+            arr[index] = arr[i];
+            arr[i] = g;
+        }
+    }
+
+    // insert linkedlist items into Array
+
+    public void loadIntoTruck() {
+ 
+
+    }
+
+}
