6e3ea8272bb6643a9cdcdd2b49b01be54de4b440
Marc Codrington <codringtonmarc@gmail.com>
2022-08-26 17:32:42 -0300

    added testing feature

---
+++ application/ApplicationTest.java
@@ -0,0 +1,26 @@
+package application;
+
+import org.junit.Test;
+
+import dataStructures.DataStructures;
+import model.Bottles;
+import model.Store;
+
+
+public class ApplicationTest {
+    
+    @Test
+    public void addToQueueTest(){
+        DataStructures d = new DataStructures();
+        Bottles bottles = new Bottles("water", "Large",10);
+        d.addBottlesInQueue(bottles);
+    }
+
+
+    @Test
+    public void addToLinkedListTest() {
+        DataStructures d = new DataStructures();
+        Store store = new Store("WinkelFred","freddielaan #7","462712",200);
+        d.addToLinkedList(store);
+    }
+}
